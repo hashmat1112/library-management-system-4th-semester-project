db407926621f1b67c061593cae88aedddc46468b
hashmat1112 <hashmatsahar471@gmail.com>
2026-05-10 12:28:53 +0500

    fix db connection timeout

--- db.py
+++ db.py
@@ -6,5 +6,7 @@ def get_connection():
         host=os.environ.get("DB_HOST", "localhost"),
         user=os.environ.get("DB_USER", "root"),
         password=os.environ.get("DB_PASS", "yourpassword"),
-        database=os.environ.get("DB_NAME", "library_db")
+        database=os.environ.get("DB_NAME", "library_db"),
+        port=int(os.environ.get("DB_PORT", 3306)),
+        connection_timeout=10
     )
